ca3409f47986c83c89dd7f621ca5f163d66f2b46
Nicolas Esquibel <n.m.esquibel01@gmail.com>
2023-09-11 20:28:32 -0700

    connected database,prompts

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+
+const { menu, addDepartment } = require('./prompts');
+const { db, getDepartments } = require('./db');
+
+const main = async () => {
+
+  let connection;
+
+  try {
+    
+    // get database connection
+    connection = await db();
+
+    let finish = false;
+
+    while(!finish) {
+
+      // prompt user for action
+      let choice = await menu();
+
+      switch(choice) {
+        case 'View All Departments':
+          const departments = await getDepartments(connection);
+          console.table(departments);
+          break;
+
+        case 'Add Department':
+          const name = await addDepartment(); 
+          // use connection to insert department  
+          break;
+
+        // other cases
+      }
+
+    }
+
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (connection) connection.end();
+  }
+
+}
+
+main();
